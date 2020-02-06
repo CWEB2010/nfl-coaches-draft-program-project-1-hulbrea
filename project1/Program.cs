b363306fa0c8967db275431a69970c8fb8f993dd
Hull <hulbrea@dunwoody.edu>
2020-02-06 10:20:35 -0600

    Added player names

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,5 +1,10 @@
-﻿//Brenna Hull
-//CWEB2010 Project 1
+﻿/*
+
+Brenna Hull
+CWEB2010 Project 1
+2/6/2020
+
+*/
 
 using System;
 
@@ -9,7 +14,38 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[,] Quarterback =
+            {
+                {"Joe Burrow", "Tua Tagoviloa", "Justin Herbert", "Jordan Love", "Jake Fromm" }
+            };
+            string[,] RunningBack =
+            {
+                {"D'Andre Smith","Jonathan Taylor", "J.X. Dobbins", "Zack Moss", "Cam Akers" }
+            };
+            string[,] WideReveiver =
+            {
+                {"CeeDee Lamb","Jerry Jeudy","Tee Higgins","Henry Ruggs III", "Tyler Johnson" }
+            };
+            string[,] DefensiveLineman =
+            {
+                {"Chase Young", "Derrick Brown", "A.J. Epensa", "Javon Kinlaw", "Yetur Gross-Matos" }
+            };
+            string[,] DefensiveBack =
+            {
+                {"Jeff Okudah", "Grant Delpit", "Kritian Fulton", "Xavier McKinney", "CJ Henderson" }
+            };
+            string[,] TightEnds =
+            {
+                {"Cole Kmet","Brycen Hopkinds","Hunter Bryant", "Jared Pinkney","Jacob Breeland" }
+            };
+            string[,] LineBacker =
+            {
+                {"Isaiah Simmons", "Kenneth Murray", "Zach Baun", "Akeem Davis-Gaither", "Troy Dye" }
+            };
+            string[,] OffensiveTackles =
+            {
+                {"Jedrick Wills Jr.", "Andrew Thomas", "Tristan Wirfs", "Tyler Biadasz", "Mekhi Becton" }
+            };
         }
     }
 }
